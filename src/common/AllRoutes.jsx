862a036b690bba39ca30b97f06a95f9ecbd23abc
suraj kumar <surajsinhakingr77>
2023-06-21 22:06:04 +0530

    done commit from the ecom site

--- src/common/AllRoutes.jsx
+++ src/common/AllRoutes.jsx
@@ -5,12 +5,14 @@ import RegistrationForm from '../components/AuthPage/RegistrationForm'
 import LoginPage from '../components/AuthPage/LoginPage'
 import ProductDetail from '../components/NewArival/ProductsItem/ProductDetail'
 import Cart from '../components/Cart/Cart'
+import Shop from '../components/shop/Shop'
 
 function AllRoutes() {
     return (
         <>
             <Routes>
                 <Route exact path="/" element={<Home/>}/>
+                <Route path="/shop" element={<Shop/>}/>
                 <Route path="/signup" element={<RegistrationForm/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/Productdetail" element={<ProductDetail/>} />
