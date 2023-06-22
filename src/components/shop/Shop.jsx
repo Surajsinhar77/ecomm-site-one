acab9de834b0d52e81b750ec3fa7bbbaa7bd529b
Suraj Kumar <surajsinhaking77@gmail.com>
2023-06-22 13:17:01 +0530

    done push and edit

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Shop() {
     return (
-        <div>
-
+        <div className='ShopContainer'>
+            <div className="productFilter w-80 border">
+                Filter Content here 
+            </div>
+            <div className="Products">
+                main Content is here 
+            </div>
         </div>
     )
 }
