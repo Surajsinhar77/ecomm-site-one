364bb7225b46d8afe30c2d1c132bf208aea29b9f
suraj kumar <surajsinhakingr77>
2023-08-10 23:20:01 +0530

    done somthing

--- src/components/AuthPage/LoginPage.jsx
+++ src/components/AuthPage/LoginPage.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+
+    const [email, setEmail] = useState('');
+    const [password, setpassword] = useState('');
+    const [error, setError] = useState('');
+
+
+    const handelSubmit = (e) => {
+        e.preventDefault();
+
+        if(email.trim === '' || password.trim ){
+            setError = "Please fill in all field"
+        }else{
+            
+        }
+    }
     return (
         <div className='mainlogindiv w-full flex items-center'>
                 <div class="w-full max-w-sm p-4 mx-auto bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                    <form class="space-y-6" action="#">
+                    <form class="space-y-6" action="#" onSubmit={(e)=>handelSubmit}>
                         <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required></input>
+                            <input type="email" name="email" id="email" onChange={(e)=>{setEmail(e.target.value)}} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required></input>
                         </div>
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required></input>
+                            <input type="password" name="password" onChange={(e)=>setpassword(e.target.value)} id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required></input>
                         </div>
                         <div class="flex items-start">
                             <div class="flex items-start">
