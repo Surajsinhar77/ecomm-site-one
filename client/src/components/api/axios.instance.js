a589761a3c66759fd83bef9badf73ca681a72361
suraj kumar <surajsinhakingr77>
2024-01-17 02:08:43 +0530

    dine

--- client/src/components/api/axios.instance.js
+++ client/src/components/api/axios.instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7000', // Set your base URL
+    baseURL: 'https://ecom-site-v7wd.onrender.com', // Set your base URL
     headers: {
         'Authorization': 'Bearer your_access_token', // Set common headers if needed
         'Content-Type': 'application/json',
